515d122ff7025f4eefea4e70360e8834ea4d2fcd
Jeevraj Rathore <jeevraj2324@gmail.com>
2022-01-25 23:14:09 +0530

    Conditionals

---
+++ cwh_16_conditionals.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class cwh_16_conditionals {
+    public static void main(String[] args) {
+        int age = 20;
+        if(age>18) {
+            System.out.println("Yes, Boy you can drive");
+        }
+        else {
+            System.out.println("No, Boy you can't drive");
+        }
+    }
+}
