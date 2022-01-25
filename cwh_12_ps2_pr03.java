fee7481a68f68c6d5dc815ddfadfd5220b45ab58
Jeevraj Rathore <jeevraj2324@gmail.com>
2022-01-25 23:11:03 +0530

    Practice set 2

---
+++ cwh_12_ps2_pr03.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+
+public class cwh_12_ps2_pr03 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number :");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>10);
+    }
+}
---
+++ cwh_12_ps2_pr04.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class cwh_12_ps2_pr04 {
+    public static void main(String[] args) {
+        float v = 10.0f;
+        float u = 6.0f;
+        float a = 1.0f;
+        float s = 5.0f;
+        float k = (v*v - u*u)/(2*a*s);
+        System.out.println(k);
+
+    }
+}
