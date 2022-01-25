7af877b0611b9f5cc81cf9b7921c9c2675855bb5
Jeevraj Rathore <jeevraj2324@gmail.com>
2022-01-25 23:04:20 +0530

    Literals

---
+++ CWH_04_literals.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        byte age = 32;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 566666888L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Jeevraj";
+        System.out.println(str);
+    }
+
+}
