7f9f9b22ba8da2ccee3d16e519eb39b554bad564
Jeevraj Rathore <jeevraj2324@gmail.com>
2022-01-25 23:17:24 +0530

    loops

---
+++ cwh_21_ch5_loops.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class cwh_21_ch5_loops {
+    public static void main(String[] args) {
+        //While lops
+        int i = 1;
+        while (i<=4){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish running the loop");
+
+    }
+}
