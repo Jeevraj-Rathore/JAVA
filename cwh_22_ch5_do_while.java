c290d2e8f0a5b9ea8cc52d9100a66a7c0a310bc7
Jeevraj Rathore <jeevraj2324@gmail.com>
2022-01-25 23:19:15 +0530

    Do While

---
+++ cwh_22_ch5_do_while.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class cwh_22_ch5_do_while {
+    public static void main(String[] args) {
+        //do while
+        int b = 0;
+        do {
+            System.out.println(b);
+            b++;
+        }while(b<5);
+        System.out.println("Finished Printing");
+    }
+}
