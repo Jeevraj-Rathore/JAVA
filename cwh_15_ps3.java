a3c4dce2d23cc6c92b716e68a136af1b2e11457b
Jeevraj Rathore <jeevraj2324@gmail.com>
2022-01-25 23:13:18 +0530

    Practice set 3

---
+++ cwh_15_ps3.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Locale;
+
+public class cwh_15_ps3 {
+    public static void main(String[] args) {
+//        String name = "Jack Parker";
+//        name = name.toLowerCase();
+//        System.out.println(name);
+//
+//        String text = "To Lower Case";
+//        text = text.replace(" ","_");
+//        System.out.println(text);
+
+//        String letter = "Dear <|name|>, Thaks a lot!";
+//        letter = letter.replace("<|name|>","Jeevraj");
+//        System.out.println(letter);
+
+//        String myString = "This string  contains doule and triple   spaces";
+//        System.out.println(myString.indexOf("  "));
+//        System.out.println(myString.indexOf("   "));
+
+        String myLetter = "Dear Harry,\n\t This Java Course is Nice.\n\t Thanks!";
+        System.out.println(myLetter);
+
+
+
+
+    }
+}
